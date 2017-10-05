8155bcc508c70fb1ca42ffc3be024d7a75b24681
jfrir2016 <juanreiszer@gmail.com>
2017-10-05 18:09:30 -0300

    actualizado

--- hola.c
+++ hola.c
@@ -1,5 +1,11 @@
-*/HOla Negro Trolo
+*/HOla Negro Trolo y cia:
 	git clone ...
 	git config --global user.email "you@example.com"
 	git config --global user.name "Your Name"
+	
+despues para subir:
+	git add (nombre del archivo) o git add . (para todos) 	//agrega
+	git commit -m "(mensaje)" 				//crea el commit
+	git push						//sube el commit a la nube
+		(ingresar usuario y contra) 
 /*
